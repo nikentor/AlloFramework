f45a716a816737b72714f30e2b8792cea6db2417
Nikentor <Nikentor@gmail.com>
2026-06-17 18:39:58 +0300

    Додав homePageTest.java

---
+++ src/test/java/homePageTest/HomePageTest.java
@@ -0,0 +1,25 @@
+package homePageTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static java.lang.Thread.sleep;
+
+public class HomePageTest {
+
+    @Test
+    public void checkAlloLogoDisplay()  throws InterruptedException{
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://allo.ua/");
+        sleep(5000);
+        WebElement alloLogoLocator = driver.findElement(By.xpath("//a[@class='v-logo']"));
+        Assert.assertTrue(alloLogoLocator.isDisplayed(), "Allo logo is visible");
+        driver.quit();
+    }
+}
