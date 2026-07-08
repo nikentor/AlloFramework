6fbfa6530a7f9a9f5811576c16f0bbe48a651eff
Nikentor <Nikentor@gmail.com>
2026-07-08 22:48:06 +0300

    Add search hairdryer test

--- src/test/java/tests/HomePageTest.java
+++ src/test/java/tests/HomePageTest.java
@@ -4,6 +4,7 @@ import basesClass.TestInit;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HomePage;
+import pages.SearchResultPage;
 
 public class HomePageTest extends TestInit {
 
@@ -15,5 +16,22 @@ public class HomePageTest extends TestInit {
         HomePage homePage = new HomePage(driver);
         openUrl(alloUrl);
         Assert.assertTrue(homePage.alloLogo().isDisplayed());
+    }
+
+    @Test
+    public void searchHairdryerTest() {
+
+        HomePage homePage = new HomePage(driver);
+        SearchResultPage searchResultPage = new SearchResultPage(driver);
+
+        String hairdryer = "Фен";
+
+        openUrl(alloUrl);
+
+        Assert.assertTrue(homePage.searchField().isDisplayed());
+        homePage.enterValuesInSearchField(hairdryer);
+        homePage.clickSearchButton();
+
+        Assert.assertTrue(searchResultPage.firstGoods().getText().contains(hairdryer), "First product does not contain 'Фен'");
     }
 }
