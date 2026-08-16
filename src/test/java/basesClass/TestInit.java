3ed9ecb5cea6f7a71f83ab1897695a2e0901e774
nikentor <nikentor@gmail.com>
2026-08-17 00:07:33 +0300

    Clean up test initialization

--- src/test/java/basesClass/TestInit.java
+++ src/test/java/basesClass/TestInit.java
@@ -3,9 +3,9 @@ package basesClass;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 public class TestInit {
 
@@ -28,25 +28,6 @@ public class TestInit {
 
         driver.manage().window().maximize();
         openUrl(alloUrl);
-
-        String source = driver.getPageSource();
-
-        System.out.println("URL: " + driver.getCurrentUrl());
-        System.out.println("TITLE: " + driver.getTitle());
-        System.out.println("SOURCE LENGTH: " + source.length());
-
-        System.out.println("HAS LOGO: " + source.contains("v-logo"));
-        System.out.println("HAS SEARCH: " + source.contains("search-form__input"));
-        System.out.println("HAS BUYERS BUTTON: " + source.contains("mh-button--open"));
-
-        System.out.println("LOGO ELEMENTS: " +
-                driver.findElements(org.openqa.selenium.By.cssSelector("[class*='v-logo']")).size());
-
-        System.out.println("SEARCH ELEMENTS: " +
-                driver.findElements(org.openqa.selenium.By.cssSelector("[id*='search-form__input']")).size());
-
-        System.out.println("BUYERS ELEMENTS: " +
-                driver.findElements(org.openqa.selenium.By.cssSelector("[class*='mh-button']")).size());
     }
 
     @AfterMethod
