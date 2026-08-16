b90100489d42fa10dc1380893b24530a2168f0df
nikentor <nikentor@gmail.com>
2026-08-16 23:00:43 +0300

    Configure Chrome for CI

--- src/test/java/basesClass/TestInit.java
+++ src/test/java/basesClass/TestInit.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class TestInit {
 
@@ -14,7 +15,13 @@ public class TestInit {
     @BeforeMethod
     public void openBrowser() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
+
         driver.manage().window().maximize();
         openUrl(alloUrl);
     }
